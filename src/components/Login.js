1d5b88d80c1c7b4360219941908d3b02e6924eda
Uriel27072005 <157824988+Uriel27072005@users.noreply.github.com>
2024-11-14 22:56:20 +0100

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import '../styles/Login.css'; // Importa el CSS correspondiente
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes manejar la lógica de inicio de sesión
+    console.log('Iniciar sesión con', email, password);
+  };
+
+  return (
+    <div className="login">
+      <h2>Iniciar Sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
